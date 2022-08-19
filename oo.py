ad56cf065e8cd8ae2deba0ce795d0469a53c486c
emmacodes <binma.emma@gmail.com>
2022-08-19 14:01:59 -0700

    complete class practice

--- oo.py
+++ oo.py
@@ -26,3 +26,55 @@ class Exam:
     def add_question(self, question):
         self.questions.append(question)
 
+    def administer(self):
+        score = 0
+        total = 0
+        for q in self.questions:
+            if q.ask_and_evaluate():
+                score += 1 
+            total += 1
+        return score / total
+
+class StudentExam:
+    def __init__(self, student, exam):
+        self.student = student
+        self.exam =  exam
+        self.score = 0
+    def take_test(self):
+        result = self.exam.administer()
+        print(result)
+        self.score = result
+
+class Quiz(Exam):
+    def administer(self):
+        score = super().administer()
+        if score >= 0.5:
+            return 1
+        else:
+            return 0
+        
+class StudentQuiz(StudentExam):
+    pass
+        
+def example():
+    quiz = Quiz("quiz")
+    set_q = Question(
+        'What is the method for adding an element to a set?',
+        '.add()')
+    quiz.add_question(set_q)
+    pwd_q = Question(
+        'What does pwd stand for?',
+        'print working directory')
+    quiz.add_question(pwd_q)
+    list_q = Question(
+        'Python lists are mutable, iterable, and what?',
+        'ordered')
+    quiz.add_question(list_q)
+    student = Student("Ella", "Lee", "San Francisco")
+    student_quiz = StudentQuiz(student, quiz)
+    student_quiz.take_test()
+
+example()
+
+        
+
